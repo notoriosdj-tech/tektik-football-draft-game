7fd8cb07ac64c99319899e89af311f4ee84f730d
notoriosdj-tech <elghayateanas@gmail.com>
2026-05-24 21:32:35 -0400

    Load V1 hotfix styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './v1-hotfix.css';
 
 createRoot(document.getElementById('root')).render(<App />);
